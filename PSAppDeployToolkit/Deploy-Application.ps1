8a7d4cbcfcc2988876c35b572a2a78e50920c611
Aaron Parker <aaron@stealthpuppy.com>
2022-04-10 19:40:42 +1000

    Update Office scrub scripts

--- PSAppDeployToolkit/Deploy-Application.ps1
+++ PSAppDeployToolkit/Deploy-Application.ps1
@@ -117,26 +117,14 @@ Try {
 		[string]$installPhase = 'Pre-Installation'
 
 		## Remove Office 2013  MSI installations
-		if (Test-Path "$envProgramFilesX86\Microsoft Office\Office15") {
-			Show-InstallationProgress "Uninstalling Microsoft Office 2013"
-			Write-Log "Microsoft Office 2013 was detected. Uninstalling..."
-			Execute-Process -FilePath "CScript.exe" -Arguments "`"$dirSupportFiles\OffScrub_O15msi.vbs`" CLIENTALL /S /Q /NoCancel" -WindowStyle Hidden -IgnoreExitCodes "1,2,3"
-		}
-
-		if (Test-Path "$envProgramFiles\Microsoft Office\Office15") {
+		if (Test-Path -Path "$envProgramFilesX86\Microsoft Office\Office15", "$envProgramFiles\Microsoft Office\Office15") {
 			Show-InstallationProgress "Uninstalling Microsoft Office 2013"
 			Write-Log "Microsoft Office 2013 was detected. Uninstalling..."
 			Execute-Process -FilePath "CScript.exe" -Arguments "`"$dirSupportFiles\OffScrub_O15msi.vbs`" CLIENTALL /S /Q /NoCancel" -WindowStyle Hidden -IgnoreExitCodes "1,2,3"
 		}
 
 		## Remove Office 2016 MSI installations
-		if (Test-Path "$envProgramFilesX86\Microsoft Office\Office16") {
-			Show-InstallationProgress "Uninstalling Microsoft Office 2016"
-			Write-Log "Microsoft Office 2016 was detected. Uninstalling..."
-			Execute-Process -FilePath "CScript.exe" -Arguments "`"$dirSupportFiles\OffScrub_O16msi.vbs`" CLIENTALL /S /Q /NoCancel" -WindowStyle Hidden -IgnoreExitCodes "1,2,3"
-		}
-
-		if (Test-Path "$envProgramFiles\Microsoft Office\Office16") {
+		if (Test-Path -Path "$envProgramFilesX86\Microsoft Office\Office16", "$envProgramFiles\Microsoft Office\Office16") {
 			Show-InstallationProgress "Uninstalling Microsoft Office 2016"
 			Write-Log "Microsoft Office 2016 was detected. Uninstalling..."
 			Execute-Process -FilePath "CScript.exe" -Arguments "`"$dirSupportFiles\OffScrub_O16msi.vbs`" CLIENTALL /S /Q /NoCancel" -WindowStyle Hidden -IgnoreExitCodes "1,2,3"
---
+++ scrub/README.md
@@ -0,0 +1,3 @@
+# Office Scrub Scripts
+
+Office uninstall and scrub scripts sources from [Deploy-OfficeClickToRun](https://github.com/OfficeDev/Office-IT-Pro-Deployment-Scripts/tree/master/Office-ProPlus-Deployment/Deploy-OfficeClickToRun)
